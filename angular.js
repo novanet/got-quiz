35ec19132708ebb7ddbbf205b418118dc9b6a6a2
Hallstein Brøtan <Hallstein.Brotan@nrk.no>
2019-04-17 08:57:14 +0200

    feat table with answers

--- angular.js
+++ angular.js
@@ -2,75 +2,35 @@
     'use strict';
 
     angular
-        .module('app', ['ngCookies'])
+        .module('app', [])
         .controller('controller', controller)
         .factory('dataservice', dataservice);
 
-    controller.$inject = ['$scope', 'dataservice', '$cookies'];
+    controller.$inject = ['dataservice'];
     dataservice.$inject = ['$q', '$http'];
 
-    function controller($scope, dataservice, $cookies) {
+    function controller(dataservice) {
         var vm = this;
         vm.answer = {};
-        vm.error = null;
-        vm.hasSubmitted = false;
-        vm.isSubmitting = false;       
-        vm.submit = submit;             
-              
-        var answerFromCookie = $cookies.get('got-quiz');
-        if(answerFromCookie)
-            vm.answer = JSON.parse(answerFromCookie); 
+        vm.error = null;     
 
-        $scope.$watchCollection('vm.answer', function() { 
-           $cookies.put('got-quiz', JSON.stringify(vm.answer), { expires: new Date(2019, 4, 15) });
-         });            
-
-        function submit(){
-            if(!vm.answer.name || !vm.answer.email || vm.answer.email.indexOf('@') === -1){
-                vm.error = "Du må fylle ut navn og gyldig epost";
-                return;
-            }                               
-            
-            vm.error = null;
-            vm.isSubmitting = true;
-            
-            dataservice.post(vm.answer)
-                .then(function(){
-                    vm.hasAnswered = true;
-                })
-                .catch(function(ex){
-					console.log(ex);
-                    vm.error = "Det har skjedd en feil ved levering av svarene. Vennligst prøv på nytt.";
-                })
-                .finally(function(){
-                    vm.isSubmitting = false;    
-                });
-        } 
+		dataservice.get()
+            .then(function(result){
+                vm.answers = result;
+            });		        
     }
 
     function dataservice($q, $http) {
         return {
-            post : post   
+			get: _.once(get)
         }
-      
-        function post(data) {
+		function get() {
             return $q(function (resolve, reject) {
-                $http.post('https://novanet-got-quiz-api.azurewebsites.net/api/answers', data)
-                    .then(saveComplete)
-                    .catch(saveFailed);
-
-                function saveComplete(response) {
-                    if (!response || !response.data.success) {
-                        reject(response.data.errorCode);
-                    } else {
-                        resolve(response.data.result);
-                    }
-                }
-
-                function saveFailed() {
-                    reject();
-                }
+                $http.get('https://novanet-got-quiz-api.azurewebsites.net/api/answers')
+                    .then(function (response) {
+                        resolve(response.data);
+                    });
             });
-        }
+        }       
     }    
 })();
